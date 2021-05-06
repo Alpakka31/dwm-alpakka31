720b2402add8225961ba66e62dac4484712a2958
Alpakka31 <patukka31@gmail.com>
2021-05-06 21:48:08 +0300

    add autostart.sh

---
+++ .dwm/autostart.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+setxkbmap fi
+dwmblocks &
+paperview /home/patrik/Ohjelmat/paperview/scenes/blue-sparkles 5 0 0 2560 1440 /home/patrik/Ohjelmat/paperview/scenes/blue-sparkles 5 2560 360 1920 1080 &
